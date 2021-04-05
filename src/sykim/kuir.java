0d2b109fc2edc26ee0adca13964186fff59678c9
Seyeon Kim <ksytpdus0304@gmail.com>
2021-04-06 04:40:11 +0900

    searcher.java에 따른 main 수정

--- src/sykim/kuir.java
+++ src/sykim/kuir.java
@@ -13,8 +13,6 @@ public class kuir {
 		// TODO Auto-generated method stub
 		
 		if (args.length == 2) {
-			System.out.println(args[0]);
-			System.out.println(args[1]);
 			if (args[0].equals("-c")) {
 				@SuppressWarnings("unused")
 				makeCollection mc = new makeCollection(args[1]);
@@ -29,10 +27,31 @@ public class kuir {
 			}
 			
 		}
+		else if (args.length == 4) {
+			if (args[0].equals("-s") && args[2].equals("-q")) {
+				searcher sc = new searcher(args[1], args[3]);
+			}
+		}
 		else {
 			System.err.println("올바른 옵션을 입력하세요");
 			System.exit(1);
 		}
+		
+		
+		/*
+		
+		zsh (cmd) 창에서 실행할 때
+		
+		kimseyeon@gimseyeon-ui-MacBookPro ~ % cd SimpleIR/src
+		kimseyeon@gimseyeon-ui-MacBookPro src % cd sykim
+		kimseyeon@gimseyeon-ui-MacBookPro sykim % javac -cp ".:../../jsoup-1.13.1.jar:../../kkma-2.1.jar" *.java
+		kimseyeon@gimseyeon-ui-MacBookPro sykim % cd ..
+		kimseyeon@gimseyeon-ui-MacBookPro src % java -cp ../jsoup-1.13.1.jar: sykim.kuir -c ../data
+		kimseyeon@gimseyeon-ui-MacBookPro src % java -cp ../kkma-2.1.jar: sykim.kuir -k Collection.xml
+		kimseyeon@gimseyeon-ui-MacBookPro src % java sykim.kuir -i index.xml
+		kimseyeon@gimseyeon-ui-MacBookPro src % java -cp ../kkma-2.1.jar: sykim.kuir -s index.post -q "라면에는 면, 분말 스프가 있다."
+		 
+		*/
 
 
 	}
